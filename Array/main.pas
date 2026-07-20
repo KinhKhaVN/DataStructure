5b906dd382a15abcf376db5b151cac65d6c30209
KinhKhaVN <pebin0123@gmail.com>
2026-07-20 10:29:37 +0700

    Refactoring

--- Array/main.pas
+++ Array/main.pas
@@ -3,9 +3,10 @@ PROGRAM Inserting;
 USES crt;
 
 CONST
-  n = 5;
+  capacity = 10;
 VAR
-  A: array[0..n - 1] of integer = (1, 2, 4, 5, 0);
+  n: integer = 5;
+  A: array[0..capacity - 1] of integer = (1, 2, 4, 5, 0, 0, 0, 0, 0, 0);
   index: integer = 0;
   i: integer = 0;
   value: integer = 0;
@@ -15,15 +16,15 @@ PROCEDURE
   VAR
     i: integer;
   BEGIN
-    for i := 0 to n - 1 do
-      Write(A[i]);
+    for i := 0 to capacity - 1 do
+      Write(A[i], ' ');
     Writeln;
   END;
 
 PROCEDURE
   Insert(VAR A:array of integer; value, index: integer);
   BEGIN
-    i := n ;
+    i := n - 1;
 
     repeat
       BEGIN
@@ -33,6 +34,7 @@ PROCEDURE
 
     until i <= index;
     A[index] := value;
+    inc(n);
   END;
 
 PROCEDURE
@@ -45,12 +47,14 @@ PROCEDURE
       A[i] := A[i + 1];
 
     A[n - 1] := 0;
+    dec(n);
   END;
 
 BEGIN
 
   Print(A);
   Insert(A, 3, 2);
+  Insert(A, 10, 2);
   Print(A);
   Remove(A, 2);
   Print(A);
