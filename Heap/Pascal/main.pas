b75aaea6d4822f39fb144b57832cefe7e9f2deae
KinhKhaVN <pebin0123@gmail.com>
2026-08-03 19:43:10 +0700

    Merge two heaps
    -Merge(dest, src), insert all elements from src into dest

--- Heap/Pascal/main.pas
+++ Heap/Pascal/main.pas
@@ -5,10 +5,22 @@ USES crt;
 CONST
   capacity = 10;
 
+TYPE
+  Heap = record
+    A: array[0..capacity - 1] of integer;
+    size: integer;
+  END;
 VAR
   n: integer = 0;
   A: array[0..capacity - 1] of integer;
   i, k: integer;
+  h1, h2: Heap;
+
+PROCEDURE
+  Init(VAR h: Heap);
+  BEGIN
+    h.size := 0;
+  END;
 
 PROCEDURE
   Swap(VAR a, b: integer);
@@ -21,66 +33,89 @@ PROCEDURE
   END;
 
 PROCEDURE
-  Print;
+  Print(VAR h: Heap);
   VAR
     i: integer;
   BEGIN
-    for i := 0 to n - 1 do
-      Write(A[i], ' ');
+    for i := 0 to h.size - 1 do
+      Write(h.A[i], ' ');
     Writeln;
   END;
 
 PROCEDURE
-  Insert(x: integer);
+  Insert(VAR h: Heap; x: integer);
   VAR
     i: integer;
   BEGIN
-    A[n] := x;
-    inc(n);
+    h.A[h.size] := x;
+    inc(h.size);
 
-    i := n - 1;
+    i := h.size - 1;
 
-    while (i > 0) AND (A[i] <= A[(i - 1) div 2]) do
+    while (i > 0) AND (h.A[i] <= h.A[(i - 1) div 2]) do
       BEGIN
-        Swap(A[i], A[(i - 1) div 2]);
+        Swap(h.A[i], h.A[(i - 1) div 2]);
         i := (i - 1) div 2;
       END;
   END;
 
 FUNCTION
-  Remove: integer;
+  Remove(VAR h: Heap): integer;
   VAR
     i, j: integer;
   BEGIN
-    Remove := A[0];
-    Swap(A[0], A[n - 1]);
-    dec(n);
+    Remove := h.A[0];
+    Swap(h.A[0], h.A[h.size - 1]);
+    dec(h.size);
     i := 0;
 
-    while 2*i + 1 < n do
+    while 2*i + 1 < h.size do
       BEGIN
         j := 2*i + 1;
-        if (2*i + 2 < n) AND (A[2*i + 2] < A[j]) then j := 2*i + 2;
+        if (2*i + 2 < h.size) AND (h.A[2*i + 2] < h.A[j]) then j := 2*i + 2;
 
-        if A[j] >= A[i] then break;
-        Swap(A[i], A[j]);
+        if h.A[j] >= h.A[i] then break;
+        Swap(h.A[i], h.A[j]);
         i := j;
       END;
 
   END;
 
+PROCEDURE
+  Merge(VAR dest, src: Heap);
+  VAR
+    i: integer;
+  BEGIN
+    for i := 0 to src.size - 1 do
+      Insert(dest, src.A[i]);
+  END;
+
 BEGIN
   CLRSCR;
 
-  Insert(10);
-  Insert(-2);
-  Insert(1);
-  Insert(2);
-  Insert(3);
-  Insert(0);
-  Insert(0);
-  Print;
+  Init(h1);
+  Init(h2);
+  
+  Insert(h1, 10);
+  Insert(h1, -2);
+  Insert(h1, 1);
 
-  for i := 0 to n - 1 do
-    Writeln(Remove);
+  Insert(h2, 2);
+  Insert(h2, 3);
+  Insert(h2, 0);
+  Insert(h2, 0);
+
+  Write('h1: ');
+  Print(h1);
+
+  Write('h2: ');
+  Print(h2);
+
+  Write('Merge: ');
+  Merge(h1, h2);
+  Print(h1);
+
+  for i := 0 to h1.size - 1 do
+    Writeln(Remove(h1));
+
 END.
